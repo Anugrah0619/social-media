d74f75b78b580ec0a997354b965a90b8a3bf4ce0
Anugrah Kulkarni <kulkarnianugrah06@gmail.com>
2026-04-16 12:21:21 +0530

    Stage 3: Text preprocessing module implemented successfully using NLTK

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from modules.preprocessing import clean_text
 
 print("🚀 File is running...")
 
@@ -6,13 +7,23 @@ def load_data():
     try:
         df = pd.read_csv("../data/sample_data.csv")
         print("✅ Data Loaded Successfully")
-        print(df.head())
-        print("Total rows:", len(df))
         return df
     except Exception as e:
         print("❌ Error loading data:", e)
         return None
 
+def preprocess_data(df):
+    df["clean_text"] = df["text"].apply(clean_text)
+    
+    print("\n🧹 Cleaned Data:")
+    print(df[["text", "clean_text"]])
+    
+    return df
+
 if __name__ == "__main__":
     print("👉 Inside main block")
+    
     df = load_data()
+    
+    if df is not None:
+        df = preprocess_data(df)
---
+++ backend/modules/preprocessing.py
@@ -0,0 +1,23 @@
+import re
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+stop_words = set(stopwords.words('english'))
+
+def clean_text(text):
+    # Remove URLs
+    text = re.sub(r"http\S+|www\S+", "", text)
+    
+    # Remove special characters
+    text = re.sub(r"[^a-zA-Z\s]", "", text)
+    
+    # Convert to lowercase
+    text = text.lower()
+    
+    # Tokenization
+    tokens = word_tokenize(text)
+    
+    # Remove stopwords
+    filtered_words = [word for word in tokens if word not in stop_words]
+    
+    return " ".join(filtered_words)
