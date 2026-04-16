7aa302e5a47503aede02bf5205470af58825c0be
Anugrah Kulkarni <kulkarnianugrah06@gmail.com>
2026-04-16 12:25:36 +0530

    Stage 4: Sentiment analysis implemented successfully using VADER

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from modules.preprocessing import clean_text
+from modules.sentiment import get_sentiment
 
 print("🚀 File is running...")
 
@@ -20,6 +21,14 @@ def preprocess_data(df):
     
     return df
 
+def analyze_sentiment(df):
+    df["sentiment"] = df["clean_text"].apply(get_sentiment)
+    
+    print("\n😊 Sentiment Analysis:")
+    print(df[["clean_text", "sentiment"]])
+    
+    return df
+
 if __name__ == "__main__":
     print("👉 Inside main block")
     
@@ -27,3 +36,4 @@ if __name__ == "__main__":
     
     if df is not None:
         df = preprocess_data(df)
+        df = analyze_sentiment(df)
